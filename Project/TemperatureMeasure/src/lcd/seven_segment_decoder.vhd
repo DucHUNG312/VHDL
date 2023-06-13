3d2fa2dc14a41fc467c696848ddf5bc58e0c88bf
DucHUNG312 <103761842+DucHUNG312@users.noreply.github.com>
2023-06-14 02:35:26 +0700

    Changed seven_segment_decoder implementation

--- Project/TemperatureMeasure/src/lcd/seven_segment_decoder.vhd
+++ Project/TemperatureMeasure/src/lcd/seven_segment_decoder.vhd
@@ -1,18 +1,16 @@
--- ==================================================================================
+-- =====================================================================================
 -- AUTHOR:          Le Vu Duc Hung
 --
 -- DATE:            13/06/2023
 --
 -- FILE:            seven_segment_decoder.vhd
 --
--- DESCRIPTION:     This file describes a seven-segment decoder component in VHDL. 
---                  The component takes an input ADC data and converts it into a 
---                  temperature value. The temperature value is then decoded into 
---                  individual digit segments for display on a seven-segment display. 
---                  The output display segment controls are provided through the 
---                  'display' port. Optionally, the 'inverted_out' generic can be 
---                  set to invert the display segments.
--- ==================================================================================
+-- DESCRIPTION:     This file contains the implementation of a seven-segment decoder
+--                  The decoder converts a 4-bit input value into the corresponding
+--                  signals to display the decimal digits 0 to 9 and the hexadecimal
+--                  letters A to F on a seven-segment display. The inverted_out generic
+--                  can be set to true to invert the output signals.   
+-- =====================================================================================
 -- MIT License
 -- Copyright (c) 2023 Le Vu Duc Hung
 --
@@ -34,71 +32,55 @@
 -- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 -- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 -- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- ==================================================================================
+-- =====================================================================================
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity seven_segment_decoder is
-    generic (
-        inverted_out: boolean := false
+    generic ( 
+        inverted_out: boolean := false 
     );
     port (
-        clk:           in std_ulogic;
-        reset:         in std_ulogic;
-        adc_data_out:  in unsigned(7 downto 0);
-        display:       out std_ulogic_vector(6 downto 0)
+        data_in:    in std_ulogic_vector(3 downto 0);
+        a:          out std_ulogic;
+        b:          out std_ulogic;
+        c:          out std_ulogic;
+        d:          out std_ulogic;
+        e:          out std_ulogic;
+        f:          out std_ulogic;
+        g:          out std_ulogic
     );
 end entity seven_segment_decoder;
 
 architecture rtl of seven_segment_decoder is
-    signal temperature:       unsigned(7 downto 0)          := (others => '0'); -- Temperature signal derived from ADC
-    signal temperature_digit: unsigned(3 downto 0)          := (others => '0'); -- Individual digit of the temperature
-    signal display_segment:   std_ulogic_vector(6 downto 0) := (others => '0'); -- Individual segment control for the display
+    signal decoder: std_ulogic_vector(6 downto 0);
 begin
-    SEVEN_SEGMENT_DECODER : process(clk)
-    begin
-        if rising_edge(clk) then
-            if reset = '1' then
-                display_segment <= (others => '0');
-            else
-                -- Convert ADC reading to temperature
-                temperature <= adc_data_out;
-
-                -- Convert temperature reading to individual digits
-                temperature_digit <= temperature(7 downto 4);
-
-                -- Decode the temperature digit to 7-segment representation
-                case temperature_digit is
-                    when "0000" =>
-                        display_segment <= "0111111";  -- Display '0'
-                    when "0001" =>
-                        display_segment <= "0000110";  -- Display '1'
-                    when "0010" =>
-                        display_segment <= "1011011";  -- Display '2'
-                    when "0011" =>
-                        display_segment <= "1001111";  -- Display '3'
-                    when "0100" =>
-                        display_segment <= "1100110";  -- Display '4'
-                    when "0101" =>
-                        display_segment <= "1101101";  -- Display '5'
-                    when "0110" =>
-                        display_segment <= "1111101";  -- Display '6'
-                    when "0111" =>
-                        display_segment <= "0000111";  -- Display '7'
-                    when "1000" =>
-                        display_segment <= "1111111";  -- Display '8'
-                    when "1001" =>
-                        display_segment <= "1100111";  -- Display '9'
-                    when others =>
-                        display_segment <= "0000000";  -- Display '-' for unknown value
-                end case;
-            end if;
-        end if;
-    end process; -- SEVEN_SEGMENT_DECODER
+    with data_in select decoder <=  "0111111" when "0000", -- 0
+                                    "0000110" when "0001", -- 1
+                                    "1011011" when "0010", -- 2
+                                    "1001111" when "0011", -- 3
+                                    "1100110" when "0100", -- 4
+                                    "1101101" when "0101", -- 5
+                                    "1111101" when "0110", -- 6
+                                    "0000111" when "0111", -- 7
+                                    "1111111" when "1000", -- 8
+                                    "1100111" when "1001", -- 9
+                                    "1110111" when "1010", -- A
+                                    "1111100" when "1011", -- B
+                                    "0111001" when "1100", -- C
+                                    "1011110" when "1101", -- D
+                                    "1111001" when "1110", -- E
+                                    "1110001" when others; -- F
 
     -- Connect IO
-    display <= not display_segment when inverted_out else display_segment;
+    a <= not decoder(0) when inverted_out else decoder(0);
+    b <= not decoder(1) when inverted_out else decoder(1);
+    c <= not decoder(2) when inverted_out else decoder(2);
+    d <= not decoder(3) when inverted_out else decoder(3);
+    e <= not decoder(4) when inverted_out else decoder(4);
+    f <= not decoder(5) when inverted_out else decoder(5);
+    g <= not decoder(6) when inverted_out else decoder(6);
 
 end architecture rtl;
